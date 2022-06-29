ea88eec5ff108408557d22ea6659b87d0153ecb9
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-06-29 18:01:53 -0300

    requisito 1 concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const answer = [];
+  // return (ids.length===0)?[]:species.filter(({id})=> ids.includes(id)); //solução da Larissa
+
+  ids.forEach((id) => { answer.push(...(species.filter((element) => id === element.id))); });
+
+  return answer;
 }
+
+// com sugestões de Artur Senna
 
 module.exports = getSpeciesByIds;
