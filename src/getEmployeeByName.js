56d37bb2a17614bdf534ccfa4a3dc210a1d28b47
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-06-29 19:03:18 -0300

    requisito 3 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const getByName = employees.find((employee) => employee.firstName === employeeName);
+  const getByLastName = employees.find((employee) => employee.lastName === employeeName);
+
+  if (getByName !== undefined) return getByName;
+  if (getByLastName !== undefined) return getByLastName;
 }
 
 module.exports = getEmployeeByName;
