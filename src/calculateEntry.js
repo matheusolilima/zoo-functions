836adf2f5e580896e8d3805efac832a13192f7e4
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-06-30 20:14:28 -0300

    lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,24 +1,26 @@
 const data = require('../data/zoo_data');
-const prices = data.prices;
+
 // com contribuição de Luis Laporta, Matheus Eduardo, Ivan Martins
+
 function countEntrants(entrants) {
-  if(!entrants || Object.keys(entrants).length === 0) return 0;
-  let saida = {};
-  saida.child = entrants.filter((entrant) => entrant.age < 18).length;
-  saida.adult = entrants.filter((entrant) => (entrant.age >= 18) && (entrant.age < 50)).length;
-  saida.senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const saida = {
+    child: entrants.filter((entrant) => entrant.age < 18).length,
+    adult: entrants.filter((entrant) => (entrant.age >= 18) && (entrant.age < 50)).length,
+    senior: entrants.filter((entrant) => entrant.age >= 50).length,
+  };
   return saida;
 }
 
 function calculateEntry(entrants) {
-  if(!entrants || Object.keys(entrants).length === 0) return 0;
-  let obj = countEntrants(entrants);
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const obj = countEntrants(entrants);
   let finalPrice = 0;
-  finalPrice += obj.adult * prices.adult;
-  finalPrice += obj.child * prices.child;
-  finalPrice += obj.senior * prices.senior;
+  finalPrice += obj.adult * data.prices.adult;
+  finalPrice += obj.child * data.prices.child;
+  finalPrice += obj.senior * data.prices.senior;
   finalPrice = finalPrice.toFixed(2);
-  return Number(finalPrice); 
+  return Number(finalPrice);
 }
 
 module.exports = { calculateEntry, countEntrants };
