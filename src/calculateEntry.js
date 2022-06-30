39473571f4bc28e538e60f6663cfc111be86fe93
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-06-30 19:38:49 -0300

    calculateEntry está com problema no filter

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,41 @@
 const data = require('../data/zoo_data');
+const prices = data.prices;
 
 function countEntrants(entrants) {
   // seu código aqui
-  // mudando o stage
+  //console.log(entrants);
+  if(!entrants) return 0;
+  if(entrants === {}) return 0;
+  let saida = {};//child: 0, adult: 0, senior: 0
+  saida.child = entrants.filter((entrant) => entrant.age < 18).length;
+  saida.adult = entrants.filter((entrant) => (entrant.age >= 18) && (entrant.age < 50)).length;
+  saida.senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  //console.log(saida);
+  return saida;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if(!entrants) return 0;
+  if(entrants === {}) return 0;
+  let obj = countEntrants(entrants);
+  let finalPrice = 0;
+
+  finalPrice += obj.adult * prices.adult;
+  finalPrice += obj.child * prices.child;
+  finalPrice += obj.senior * prices.senior;
+  finalPrice = finalPrice.toFixed(2);
+  console.log(finalPrice);
+  return Number.finalPrice; 
 }
 
+let entrada = [
+{ name:  'Lara Carvalho', age:  5 },
+{ name:  'Frederico Moreira', age:  5 },
+{ name:  'Pedro Henrique Carvalho', age:  5 },
+{ name:  'Maria Costa', age:  18 },
+{ name:  'Núbia Souza', age:  18 },
+{ name:  'Carlos Nogueira', age:  50 },]
+//calculateEntry(entrada);
+console.log(countEntrants(entrada));
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se o argumento "count" faz a função retornar a quantidade de elefantes',() => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verifica se o argumento "names" faz a função retornar um array com a relação dos nomes de todos os elefantes',() => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Verifica se o argumento "averageAge" faz a função retornar a média de idade dos elefantes',() => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('Verifica se o argumento "location" faz a função retornar a localização dos elefantes dentro do Zoológico',() => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Verifica se o argumento "popularity" faz a função retornar a popularidade de elefantes',() => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Verifica se o argumento "availability" faz a função retornar um array com a relação de dias em que é possível visitar os elefantes',() => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
