ac27c6dbc2c93e419a5efd3d1b6639982a3b5540
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-06-30 16:19:08 -0300

    requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  let saida = {};
+  if (!animal) {
+    species.forEach((specie) => { saida[specie.name] = specie.residents.length; });
+    return saida;
+  }
+  if (!animal.sex) {
+    saida = species.find((specie) =>
+      specie.name === animal.specie).residents.length;
+    return saida;
+  }
+  saida = species.find((specie) =>
+    specie.name === animal.specie).residents.filter((resident) =>
+    resident.sex === animal.sex).length;
+  return saida;
 }
-
 module.exports = countAnimals;
