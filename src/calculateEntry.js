f3d77b2d90a7d69ebd57f4934ea58d0dfc1a4e1b
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-06-30 19:59:40 -0300

    requisito 8 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,41 +1,24 @@
 const data = require('../data/zoo_data');
 const prices = data.prices;
-
+// com contribuição de Luis Laporta, Matheus Eduardo, Ivan Martins
 function countEntrants(entrants) {
-  // seu código aqui
-  //console.log(entrants);
-  if(!entrants) return 0;
-  if(entrants === {}) return 0;
-  let saida = {};//child: 0, adult: 0, senior: 0
+  if(!entrants || Object.keys(entrants).length === 0) return 0;
+  let saida = {};
   saida.child = entrants.filter((entrant) => entrant.age < 18).length;
   saida.adult = entrants.filter((entrant) => (entrant.age >= 18) && (entrant.age < 50)).length;
   saida.senior = entrants.filter((entrant) => entrant.age >= 50).length;
-  //console.log(saida);
   return saida;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
-  if(!entrants) return 0;
-  if(entrants === {}) return 0;
+  if(!entrants || Object.keys(entrants).length === 0) return 0;
   let obj = countEntrants(entrants);
   let finalPrice = 0;
-
   finalPrice += obj.adult * prices.adult;
   finalPrice += obj.child * prices.child;
   finalPrice += obj.senior * prices.senior;
   finalPrice = finalPrice.toFixed(2);
-  console.log(finalPrice);
-  return Number.finalPrice; 
+  return Number(finalPrice); 
 }
 
-let entrada = [
-{ name:  'Lara Carvalho', age:  5 },
-{ name:  'Frederico Moreira', age:  5 },
-{ name:  'Pedro Henrique Carvalho', age:  5 },
-{ name:  'Maria Costa', age:  18 },
-{ name:  'Núbia Souza', age:  18 },
-{ name:  'Carlos Nogueira', age:  50 },]
-//calculateEntry(entrada);
-console.log(countEntrants(entrada));
 module.exports = { calculateEntry, countEntrants };
