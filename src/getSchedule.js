8b372bfcb981e006a8a04631e94874bb5c815850
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-07-01 19:43:53 -0300

    getSchedule melhoradaquase  aprova lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,6 +1,18 @@
 const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 // com uma dica de Marcos Vinicius
+
+function getDay(scheduleTarget, saida) {
+  if (scheduleTarget === 'Monday') return { Monday: saida[scheduleTarget] };
+  const obj = {};
+  obj[scheduleTarget] = {
+    officeHour: `Open from ${hours[scheduleTarget].open}am until ${hours[scheduleTarget].close}pm`,
+    exhibition: species.filter((especie) =>
+      especie.availability.includes(scheduleTarget)).map(({ name }) => name),
+  };
+  return obj;
+}
+
 function getSchedule(scheduleTarget) {
   const horarios = Object.entries(hours);
   const saida = {};
@@ -11,18 +23,10 @@ function getSchedule(scheduleTarget) {
         especie.availability.includes(dia[0])).map(({ name }) => name),
     });
   saida.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
-  const existeDia = horarios.some((dia) => dia[0] === scheduleTarget);
   const existeEspecie = species.filter((specie) => specie.name === scheduleTarget);
   if (existeEspecie.length > 0) return existeEspecie[0].availability;
-  if (existeDia === true) {
-    if (scheduleTarget === 'Monday') return { Monday: saida[scheduleTarget] };
-    return {
-      [scheduleTarget]: {
-        officeHour: `Open from ${hours[scheduleTarget].open}am until ${hours[scheduleTarget].close}pm`,
-        exhibition: species.filter((especie) =>
-          especie.availability.includes(scheduleTarget)).map(({ name }) => name),
-      },
-    };
+  if (horarios.some((dia) => dia[0] === scheduleTarget) === true) {
+    return getDay(scheduleTarget, saida);
   }
   return saida;
 }
