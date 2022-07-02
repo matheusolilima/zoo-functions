bd4b2a43ec72487ccfed7d2df0e65df3e4edb3aa
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-07-01 21:41:05 -0300

    requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// com dica do Marcos Vinicius
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colab = employees.find((employee) => employee.id === id);
+  const specieId = colab.responsibleFor[0];
+  const animals = species.find((specie) => specie.id === specieId);
+  const biggest = (bigger, number) => ((bigger.age > number.age) ? bigger : number);
+  const oldest = animals.residents.reduce(biggest);
+  const saida = Object.values(oldest);
+  return saida;
 }
 
 module.exports = getOldestFromFirstSpecies;
