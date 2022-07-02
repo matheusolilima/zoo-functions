ee8cb7e43a6ebaddcfe0cc2ab1078fa0159c27fc
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-07-02 18:32:58 -0300

    REQUISITO 9 DEMOLIDO

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,16 +1,17 @@
 const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-// com uma dica de Marcos Vinicius
+// com uma dica de Marcos Vinicius, e Ivan Martins
 
 function getSchedule(scheduleTarget) {
   const horarios = Object.entries(hours);
   const saida = {};
-  horarios.forEach((dia) =>
+  horarios.forEach((dia) => {
     saida[dia[0]] = {
       officeHour: `Open from ${dia[1].open}am until ${dia[1].close}pm`,
       exhibition: species.filter((especie) =>
         especie.availability.includes(dia[0])).map(({ name }) => name),
-    });
+    };
+  });
   saida.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
   const existeEspecie = species.filter((specie) => specie.name === scheduleTarget);
   if (existeEspecie.length > 0) return existeEspecie[0].availability;
