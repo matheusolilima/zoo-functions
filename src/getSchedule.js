d3a740faca55484f2f21e99804bd352c2d16e1ff
Matheus Oliveira <matheus.o.lima.95@gmail.com>
2022-07-02 18:28:37 -0300

    getSchedule linda, so falta arrumar arrow function

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,17 +2,6 @@ const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 // com uma dica de Marcos Vinicius
 
-function getDay(scheduleTarget, saida) {
-  if (scheduleTarget === 'Monday') return { Monday: saida[scheduleTarget] };
-  const obj = {};
-  obj[scheduleTarget] = {
-    officeHour: `Open from ${hours[scheduleTarget].open}am until ${hours[scheduleTarget].close}pm`,
-    exhibition: species.filter((especie) =>
-      especie.availability.includes(scheduleTarget)).map(({ name }) => name),
-  };
-  return obj;
-}
-
 function getSchedule(scheduleTarget) {
   const horarios = Object.entries(hours);
   const saida = {};
@@ -26,7 +15,8 @@ function getSchedule(scheduleTarget) {
   const existeEspecie = species.filter((specie) => specie.name === scheduleTarget);
   if (existeEspecie.length > 0) return existeEspecie[0].availability;
   if (horarios.some((dia) => dia[0] === scheduleTarget) === true) {
-    return getDay(scheduleTarget, saida);
+    if (scheduleTarget === 'Monday') return { Monday: saida.Monday };
+    return { [scheduleTarget]: saida[scheduleTarget] };
   }
   return saida;
 }
